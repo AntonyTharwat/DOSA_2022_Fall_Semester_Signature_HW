264d8b7c36465f9f672aa5118059d11549f97163
Antony_Megallla <84070221+AntonyTharwat@users.noreply.github.com>
2022-12-10 19:42:05 +0100

    Update person.cs

--- RestaurantManagementHomeWork4/person.cs
+++ RestaurantManagementHomeWork4/person.cs
@@ -1,7 +1,8 @@
-﻿namespace RestaurantManagementHomeWork4
+namespace RestaurantManagementHomeWork4
 {
     class person
     {
+        // Automatic Properties (Short Hand) for get and st each attribute
         public string personName { get; set; }
         public string phoneNumber { get; set; }
         public int noOfCompanions { get; set; }
